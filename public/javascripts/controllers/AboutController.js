6c38993a1c1232d6d4a7b7dec11468ce138e0b27
Egan Bernardino <eegun19@yahoo.com>
2016-09-09 09:58:21 -0700

    designed about page

--- public/javascripts/controllers/AboutController.js
+++ public/javascripts/controllers/AboutController.js
@@ -12,6 +12,13 @@
     vm.goHome = goHome
     vm.image = "../images/elotegan.jpg"
     vm.about = "This web application was designed and developed by Egan Bernardino, using the MEAN stack. The idea stemmed from the desire to build something that my church band can use during our services. Although, it targets a specific community, I hope to have it available for use by and person or group of people."
+    vm.contact = [
+
+      "number: 213.446.2069",
+      "email: eganpb19@gmail.com",
+      "LinkedIn: https://www.linkedin.com/home?trk=nav_responsive_tab_home"
+
+    ]
 
     function goHome() {
       $state.go("home")
