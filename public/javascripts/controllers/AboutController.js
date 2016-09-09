3ac2298c15cae8423fd8ce1c2fa2f9021fce413b
Egan Bernardino <eegun19@yahoo.com>
2016-09-09 10:13:15 -0700

    updated styling of about page

--- public/javascripts/controllers/AboutController.js
+++ public/javascripts/controllers/AboutController.js
@@ -14,8 +14,8 @@
     vm.about = "This web application was designed and developed by Egan Bernardino, using the MEAN stack. The idea stemmed from the desire to build something that my church band can use during our services. Although, it targets a specific community, I hope to have it available for use by and person or group of people."
     vm.contact = [
 
-      "number: 213.446.2069",
-      "email: eganpb19@gmail.com",
+      "Number: 213.446.2069",
+      "Email: eganpb19@gmail.com",
       "LinkedIn: https://www.linkedin.com/home?trk=nav_responsive_tab_home"
 
     ]
